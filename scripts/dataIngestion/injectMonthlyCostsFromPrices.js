fa17ece0a81ebea26bbcab5df6e9671f77877d51
Rudi Hinds <rudihinds@gmail.com>
2024-03-08 22:58:13 +0000

    clean up monthly costs script and add main city level functionality to complete

--- scripts/dataIngestion/injectMonthlyCostsFromPrices.js
+++ scripts/dataIngestion/injectMonthlyCostsFromPrices.js
@@ -52,31 +52,24 @@ const newYork = numbeoCityPricesActual.find(
   (item) => item.name === "Cairo, Egypt",
 );
 
-getCityPricesFromNumbeoJson = async () => {
-  let missingItems = newYork.prices
+const getCityMissingPricesFromNumbeoJson = async (city) => {
+  let missingItems = city.prices
     .filter(
       (priceObj) => !itemsPurchaseFrequency.hasOwnProperty(priceObj.item_name),
     )
     .map((priceObj) => priceObj.item_name);
-  console.log(missingItems);
+  return missingItems;
 };
 
-const getCitiesThatHaveAllPricingData = () => {
+const getCitiesThatHaveAllPricingData = (cityData, length) => {
   const citiesWithPrices = [];
-  for (const city of numbeoCityPricesActual) {
-    if (city.prices.length > 200) {
+  for (const city of cityData) {
+    if (city.prices.length >= length) {
       citiesWithPrices.push(city);
     }
   }
   return citiesWithPrices;
 };
-
-//if an item name from our purchase frequency data is missing from any one city's prices, make a map of all the cities within the same country
-// and then check if the item is available in any of the other cities in the same country
-// if it is, then we can use the price from that city for the missing item
-// if it isnt, make a note of the missing items and add to database for later
-// lets create an object for each city that has cityCountry, totalMonthlyCosts for each social class, and the missing items if any
-// lets start with 10 and review
 
 function createFamily(adults, children) {
   return new Array(adults)
@@ -88,76 +81,32 @@ const single = createFamily(1, 0);
 const couple = createFamily(2, 0);
 const smallFamily = createFamily(2, 2);
 const largeFamily = createFamily(2, 4);
-
-// "Meal for 2 People, Mid-range Restaurant, Three-course, Restaurants", "Apartment (1 bedroom) in City Centre, Rent Per Month"
-// excluded items:
-// "Apartment (1 bedroom) in City Centre, Rent Per Month"
-// "Apartment (1 bedroom) Outside of Centre, Rent Per Month"
-// "Apartment (3 bedrooms) in City Centre, Rent Per Month"
-// "Apartment (3 bedrooms) Outside of Centre, Rent Per Month"
-
+let familyTypes = {
+  single,
+  couple,
+  smallFamily,
+  largeFamily,
+};
 const excludedItems = [
   "Apartment (1 bedroom) in City Centre, Rent Per Month",
   "Apartment (1 bedroom) Outside of Centre, Rent Per Month",
   "Apartment (3 bedrooms) in City Centre, Rent Per Month",
   "Apartment (3 bedrooms) Outside of Centre, Rent Per Month",
 ];
-
 const socialClasses = ["workingClassFreq", "middleClassFreq", "highValueFreq"];
 
-// if (
-//   priceObj.average_price !== 0 &&
-//   itemsPurchaseFrequency[priceObj.item_name] &&
-//   itemsPurchaseFrequency[priceObj.item_name][currentFamilyMember] &&
-//   itemsPurchaseFrequency[priceObj.item_name][currentFamilyMember][
-//     socialClass
-//   ] !== 0 &&
-//   familyMemberIndex > 0 &&
-//   !excludedItems.includes(priceObj.item_name)
-// )
 const calculateFamilyMemberMonthlyCosts = (
   city,
   acc,
   currentFamilyMember,
   socialClass,
-  familyMemberIndex,
 ) => {
   let familyMemberDataUnaccountedFor = [];
   city.prices.forEach((priceObj) => {
-    if (!priceObj.item_name) {
-      console.log(`No item name ${priceObj.item_name}`);
-    }
-    // console.log("item name: ", priceObj.item_name);
-    if (priceObj.average_price === 0) {
-      // console.log(`Average price for ${priceObj.item_name} is 0`);
-    }
-    // console.log("average price: ", priceObj.average_price);
     if (!itemsPurchaseFrequency[priceObj.item_name]) {
       console.log(`No purchase frequency data for ${priceObj.item_name}`);
       familyMemberDataUnaccountedFor.push(priceObj.item_name);
       return;
-    }
-    // console.log(
-    //   "purchase frequency" + ` for ${priceObj.item_name}`,
-    //   itemsPurchaseFrequency[priceObj.item_name],
-    // );
-    if (!itemsPurchaseFrequency[priceObj.item_name][currentFamilyMember]) {
-      console.log(
-        `No purchase frequency data for ${priceObj.item_name} for family member ${currentFamilyMember}`,
-      );
-    }
-    // console.log(
-    //   `Purchase frequency for ${priceObj.item_name} for family member ${currentFamilyMember}`,
-    //   itemsPurchaseFrequency[priceObj.item_name][currentFamilyMember],
-    // );
-    if (
-      itemsPurchaseFrequency[priceObj.item_name][currentFamilyMember][
-        socialClass
-      ] === 0
-    ) {
-      // console.log(
-      //   `Purchase frequency for ${priceObj.item_name} for family member ${currentFamilyMember} in social class ${socialClass} is 0`,
-      // );
     }
     if (excludedItems.includes(priceObj.item_name)) {
       console.log(`${priceObj.item_name} is in the list of excluded items`);
@@ -172,28 +121,11 @@ const calculateFamilyMemberMonthlyCosts = (
             socialClass
           ].toFixed(2),
         );
-      if (currentFamilyMember === "adult") {
-        console.log(
-          "*************************** total start ***************************",
-        );
-        console.log("family member", currentFamilyMember);
-        console.log(
-          priceObj.item_name,
-          "item price: ",
-          priceObj.average_price.toFixed(2),
-        );
-        // console.log("item price", priceObj.average_price.toFixed(2));
-        console.log(
-          "frequency per month: ",
-          itemsPurchaseFrequency[priceObj.item_name][currentFamilyMember][
-            socialClass
-          ].toFixed(2),
-        );
-        console.log("monthly total for item: ", monthlyTotalForItem);
-      }
       acc += monthlyTotalForItem;
     } catch (error) {
-      console.error(`An error occurred: ${error.message}`);
+      console.error(
+        `Error trying to calculate monthly total for item: ${error.message}`,
+      );
     }
   });
   return [acc, familyMemberDataUnaccountedFor];
@@ -244,11 +176,7 @@ const calculateRent = (familyMembers, city, socialClass) => {
 };
 
 const getCostsForSocialClass = (city, socialClass, familyMembers) => {
-  // loop through family members, get sum of each price item for each family member by social class frequency
-  // add together to get total monthly cost for that family
-  // return total monthly cost
-
-  // add all family members living costs
+  // add all family members living costs at given social class
   let totalMonthlyCostsForFamilyExclRent = familyMembers.reduce(
     (acc, currentFamilyMember, i) => {
       let [total, familyMemberDataUnaccountedFor] =
@@ -265,51 +193,24 @@ const getCostsForSocialClass = (city, socialClass, familyMembers) => {
     },
     0,
   );
-  // add rent
-  let apartmentSize;
+  let rentCost = calculateRent(familyMembers, city, socialClass);
+  return totalMonthlyCostsForFamilyExclRent + rentCost;
 };
 
-// console.log(
-//   "grand total",
-//   getCostsForSocialClass(newYork, "workingClassFreq", couple),
-// );
+const getCostsForCity = (city) => {
+  let cityCosts = {};
+  let cityCostsBySocialClass = socialClasses.reduce((acc, socialClass) => {
+    for (const familyType in familyTypes) {
+      acc[socialClass][familyType] = getCostsForSocialClass(
+        city,
+        socialClass,
+        familyTypes[familyType],
+      );
+    }
+    return acc;
+  }, {});
+  cityCosts[city.name] = cityCostsBySocialClass;
+  return cityCosts;
+};
 
-// const checkFrequencyData = () => {
-//   for (itemKey in itemsPurchaseFrequency) {
-//     let item = itemsPurchaseFrequency[itemKey];
-//     console.log(item.child == undefined)
-//   }
-// };
-// checkFrequencyData()
-
-// createMonthlyCosts("new york", largeFamily);
-
-// console.log('numbeo json length: ', numbeoCityPricesActual.length);
-// const fiftyCities = getCitiesThatHaveAllPricingData().slice(0, 500);
-
-// fiftyCities.forEach((city) => {
-//   let itemPricesForCity = city.prices.map((priceObj) => priceObj.item_name);
-//   let missingItems = Object.keys(itemsPurchaseFrequency).filter(
-//     (item) => !itemPricesForCity.includes(item),
-//   );
-//   console.log(city.name);
-//   console.log(missingItems);
-// });
-
-// getCityPricesFromNumbeoJson();
-// console.log(Object.keys(itemsPurchaseFrequency).length);
-
-// getCityPrices().then((citiesWithPrices) => {
-//   citiesWithPrices.forEach((city) => {
-//     for (const priceObj of city.prices) {
-//       if (
-//         Object.keys(itemsPurchaseFrequency) &&
-//         !Object.keys(itemsPurchaseFrequency).includes(priceObj.item_name)
-//       ) {
-//         console.log("yolo");
-//       }
-//     }
-//   });
-// });
-
-// console.log(Object.keys(itemsPurchaseFrequency));
+const fiftyCities = getCitiesThatHaveAllPricingData().slice(0, 1`0);
